9d6d8fdd658df1eabeef2bdd10f53b79fc481ce4
kavinsood <kavinsood15@gmail.com>
2025-06-24 13:53:29 +0530

    vite build updated

--- utils/vite.js
+++ utils/vite.js
@@ -2,62 +2,61 @@ import fs from 'fs/promises'
 import path from 'path'
 import memoize from 'lodash/memoize.js'
 
-const DEFAULT_ENTRY_FILE = 'src/assets/scripts/main.js'
-const PATH_PREFIX = '/'
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const VITE_DEV_SERVER = 'http://localhost:5173'
+const MANIFEST_PATH = path.resolve(
+    process.cwd(),
+    'dist/.vite',
+    'manifest.json'
+)
+const DEFAULT_ENTRY = 'src/assets/scripts/main.js'
 
-const getAssetManifest = memoize(async function () {
-    const manifest = await fs.readFile(
-        path.resolve(process.cwd(), 'dist/.vite', 'manifest.json')
-    )
+const getManifest = memoize(async () => {
+    if (!IS_PRODUCTION) return {}
+    const manifest = await fs.readFile(MANIFEST_PATH)
     return JSON.parse(manifest)
 })
 
-async function getChunkInformationFor(entryFilename) {
-    // We want an entryFilename, because in practice you might have multiple entrypoints
-    // This is similar to how you specify an entry in development more
-    if (!entryFilename) {
-        throw new Error(
-            'You must specify an entryFilename, so that vite-script can find the correct file.'
-        )
-    }
+async function getUrls(entryFilename) {
+    const manifest = await getManifest()
+    const entry = entryFilename || DEFAULT_ENTRY
 
-    const manifest = await getAssetManifest()
-    const entryChunk = manifest[entryFilename]
-
-    if (!entryChunk) {
+    if (!manifest[entry]) {
         const possibleEntries = Object.values(manifest)
             .filter((chunk) => chunk.isEntry === true)
             .map((chunk) => `"${chunk.src}"`)
             .join(`, `)
         throw new Error(
-            `No entry for ${entryFilename} found in dist/.vite/manifest.json. Valid entries in manifest: ${possibleEntries}`
+            `No entry for ${entry} found in manifest.json. Valid entries: ${possibleEntries}`
         )
     }
 
-    return entryChunk
-}
-
-async function viteScriptTag(entryFilename) {
-    const entryFile = entryFilename || DEFAULT_ENTRY_FILE
-    const entryChunk = await getChunkInformationFor(entryFile)
-    return `<script type="module" src="${PATH_PREFIX}${entryChunk.file}"></script>`
-}
-
-async function viteLinkStylesheetTags(entryFilename) {
-    const entryFile = entryFilename || DEFAULT_ENTRY_FILE
-    const entryChunk = await getChunkInformationFor(entryFile)
-    if (!entryChunk.css || entryChunk.css.length === 0) {
-        console.warn(
-            `No css found for ${entryFilename} entry. Is that correct?`
-        )
-        return ''
+    const { file, css } = manifest[entry]
+    return {
+        js: `/${file}`,
+        css: css?.map((file) => `/${file}`) || []
     }
-    /* There can be multiple CSS files per entry, so assume many by default */
-    return entryChunk.css
-        .map(
-            (cssFile) =>
-                `<link rel="stylesheet" href="${PATH_PREFIX}${cssFile}"></link>`
-        )
+}
+
+async function viteScriptTag(entry) {
+    if (!IS_PRODUCTION) {
+        return `
+    <script type="module" src="${VITE_DEV_SERVER}/@vite/client"></script>
+    <script type="module" src="${VITE_DEV_SERVER}/${
+        entry || DEFAULT_ENTRY
+    }"></script>
+    `
+    }
+    const { js } = await getUrls(entry)
+    return `<script type="module" src="${js}" defer></script>`
+}
+
+async function viteLinkStylesheetTags(entry) {
+    if (!IS_PRODUCTION) return '' // In dev, Vite handles CSS injection
+
+    const { css } = await getUrls(entry)
+    return css
+        .map((file) => `<link rel="stylesheet" href="${file}">`)
         .join('\n')
 }
 
